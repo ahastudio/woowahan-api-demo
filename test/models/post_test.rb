3461a183a36c8dbb97af6ea058892fc28fbc28d2
Ashal aka JOKER <ahastudio@gmail.com>
2016-01-02 02:31:27 +0900

    Validation에 대한 테스트 코드 작성
    A Guide to Testing Rails Applications
    http://guides.rubyonrails.org/testing.html

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  test 'create a new post' do
-    post = Post.new
-    post.title = '조은 글이다'
-    post.body = '...'
-    assert post.save
+  test 'create a new post with valid attributes' do
+    assert_difference -> { Post.count }, 1 do
+      Post.create(title: '조은 글이다', body: '냉무')
+    end
   end
 
   test 'cannot create a new post without title' do
-    post = Post.new
-    post.body = '...'
-    assert_not post.save
+    assert_no_difference -> { Post.count } do
+      Post.create(title: '', body: '냉무')
+    end
   end
 
   test 'cannot create a new post without body' do
-    post = Post.new
-    post.title = 'xxx'
-    assert_not post.save
+    assert_no_difference -> { Post.count } do
+      Post.create(title: '조은 글이다', body: '')
+    end
+  end
+
+  test 'cannot create a new post without title and body' do
+    assert_no_difference -> { Post.count } do
+      Post.create(title: '', body: '')
+    end
   end
 end
