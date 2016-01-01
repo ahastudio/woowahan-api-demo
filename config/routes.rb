3aaf87142cac6fa36db810c381595edf43b98304
Ashal aka JOKER <ahastudio@gmail.com>
2016-01-02 07:17:23 +0900

    게시물 상세 API
    GET /api/posts/<id>.json

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :posts
 
   namespace :api do
-    resources :posts, only: :index
+    resources :posts, only: [:index, :show]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
